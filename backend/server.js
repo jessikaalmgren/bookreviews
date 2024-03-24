ea3cb80af4771297d8eea128e9d2ee55f004661a
Jessika Almgren <jessikaalmgren@MacBook-Pro-som-tillhor-Jessika.local>
2024-03-24 21:02:05 +0100

    Created endpoints for the API

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 require('dotenv').config()
 
 const express = require('express') 
+const bookRoutes = require('./routes/books')
 
-//express app
+//express app 
 const app = express()
 
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get('/', (req, res) => {
-    res.json({message: 'Welcome to the app'})
-})
+app.use('/api/books/', bookRoutes)
 
 
 //listen for requests
